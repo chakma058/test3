00839713581a5851830ffde9aeffd5e8ea6c6172
Emon <chakma25-058@diu.edu.bd>
2024-02-12 23:13:40 +0600

    done

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Info app',
+      home: WeatherPage(),
+    );
+  }
+}
+
+class WeatherPage extends StatelessWidget {
+  final String jsonString = '''
+  [
+    {
+      "city": "New York",
+      "temperature": 20,
+      "condition": "Clear",
+      "humidity": 60,
+      "windSpeed": 5.5
+    },
+    {
+      "city": "Los Angeles",
+      "temperature": 25,
+      "condition": "Sunny",
+      "humidity": 50,
+      "windSpeed": 6.8
+    },
+    {
+      "city": "London",
+      "temperature": 15,
+      "condition": "Partly Cloudy",
+      "humidity": 70,
+      "windSpeed": 4.2
+    },
+    {
+      "city": "Tokyo",
+      "temperature": 28,
+      "condition": "Rainy",
+      "humidity": 75,
+      "windSpeed": 8.0
+    },
+    {
+      "city": "Sydney",
+      "temperature": 22,
+      "condition": "Cloudy",
+      "humidity": 55,
+      "windSpeed": 7.3
+    }
+  ]
+  ''';
+
+  @override
+  Widget build(BuildContext context) {
+    List<dynamic> weatherData = json.decode(jsonString);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather Info App'),
+      ),
+      body: ListView.builder(
+        itemCount: weatherData.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(weatherData[index]['city']),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Temperature: ${weatherData[index]['temperature']}°C'),
+                  Text('Condition: ${weatherData[index]['condition']}'),
+                  Text('Humidity: ${weatherData[index]['humidity']}%'),
+                  Text('Wind Speed: ${weatherData[index]['windSpeed']} m/s'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
